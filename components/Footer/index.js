fb42c9640d8c77f35cf2f1c991f46bbadc191620
이승준 <actav@iseungjun-ui-MacBookAir.local>
2023-05-18 23:20:05 +0900

    modified some texts

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,26 +1,38 @@
 import classNames from "classnames";
 import styles from "../../styles/Footer.module.css";
 
+// 상호 : 메모리레인 | 대표 : 심원보 | 사업자등록번호 : 178-13-01961 | CS주소: 부산 북구 금곡대로 268 402-2101
+// 통신판매업신고번호 : 2023-부산북구-0383호 | 전화번호 : 0507-1360-5443 | 이메일 : acin211@naver.com
+// COPYRIGHT ⓒ 2023MEMORYRAIN. ALL RIGHT RESERVED
+
 const Footer = () => {
     return (
         <footer className={classNames(styles.footer, "width-setter")}>
-            <div className={styles.grid}>
-                <div className={styles.gridItem}>
-                    <div className={styles.gridTitle}>
-                        <i className={classNames(styles.telIco, styles.ico)}></i>
-                        Tel
+            <div className={styles.container}>
+                <div className={styles.wrapper}>
+                    <div>
+                        <div className={styles.title}>
+                            상호: 메모리레인 | 대표: 심원보
+                        </div>
+                        <div className={styles.title}>
+                            사업자등록번호 : 178-13-01961
+                        </div>
+                        <div className={styles.title}>
+                            CS주소: 부산 북구 금곡대로 268 402-2101
+                        </div>
+                        <div className={styles.title}>
+                            통신판매업신고번호 : 2023-부산북구-0383호
+                        </div>
+                        <div className={styles.title}>
+                            전화번호 : 0507-1360-5443
+                        </div>
+                        <div className={styles.title}>
+                            이메일 : acin211@naver.com
+                        </div>
+                        <div className={styles.title}>
+                            COPYRIGHT ⓒ 2023MEMORYRAIN. ALL RIGHT RESERVED
+                        </div>
                     </div>
-                    <div className={styles.gridContent}>010-3620-5446</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.gridTitle}>
-                        <i className={classNames(styles.emailIco, styles.ico)}></i>
-                        Email
-                    </div>
-                    <div className={styles.gridContent}>acin211@gmail.com</div>
-                </div>
-                <div className={styles.gridItem}>
-                    <div className={styles.gridContent}>© 2023 by MemoryRain</div>
                 </div>
             </div>
         </footer>
